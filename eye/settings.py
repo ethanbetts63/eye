4826af5b03e07eefe646c7329afa8dd99c17b960
Ethan Betts <you@example.com>
2026-01-24 11:29:13 +0100

    checkpoint

--- eye/settings.py
+++ eye/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,6 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# URL path for static files
+STATIC_URL = '/static/'
+
+# Files will be collected here when running collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
